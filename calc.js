b7a7877458d7fd69b9081af408bf30995f476512
如云泊 <254758318@qq.com>
2020-04-15 23:47:41 +0800

    能够输入0-9数字

--- calc.js
+++ calc.js
@@ -3,3 +3,120 @@ function init() {
     num.value = 0;
     num.disabled = "disabled";
 }
+
+function num_1_click() {
+    var num = document.getElementById("num");
+    var n = num.value;
+    //清除前导的数字0
+    // if (n == "0") {
+    //     n = "1";
+    // } else {
+    //     n = n + "1";
+    // }
+    n = n + "1";
+    document.getElementById("num").value = n * 1;
+}
+
+function num_2_click() {
+    var num = document.getElementById("num");
+    var n = num.value;
+    //清除前导的数字0
+    // if (n == "0") {
+    //     n = "1";
+    // } else {
+    //     n = n + "1";
+    // }
+    n = n + "2";
+    document.getElementById("num").value = n * 1;
+}
+
+function num_3_click() {
+    var num = document.getElementById("num");
+    var n = num.value;
+    //清除前导的数字0
+    // if (n == "0") {
+    //     n = "1";
+    // } else {
+    //     n = n + "1";
+    // }
+    n = n + "3";
+    document.getElementById("num").value = n * 1;
+}
+
+function num_4_click() {
+    var num = document.getElementById("num");
+    var n = num.value;
+    //清除前导的数字0
+    // if (n == "0") {
+    //     n = "1";
+    // } else {
+    //     n = n + "1";
+    // }
+    n = n + "4";
+    document.getElementById("num").value = n * 1;
+}
+
+function num_5_click() {
+    var num = document.getElementById("num");
+    var n = num.value;
+    //清除前导的数字0
+    // if (n == "0") {
+    //     n = "1";
+    // } else {
+    //     n = n + "1";
+    // }
+    n = n + "5";
+    document.getElementById("num").value = n * 1;
+}
+
+function num_6_click() {
+    var num = document.getElementById("num");
+    var n = num.value;
+    //清除前导的数字0
+    // if (n == "0") {
+    //     n = "1";
+    // } else {
+    //     n = n + "1";
+    // }
+    n = n + "6";
+    document.getElementById("num").value = n * 1;
+}
+
+function num_7_click() {
+    var num = document.getElementById("num");
+    var n = num.value;
+    //清除前导的数字0
+    // if (n == "0") {
+    //     n = "1";
+    // } else {
+    //     n = n + "1";
+    // }
+    n = n + "7";
+    document.getElementById("num").value = n * 1;
+}
+
+function num_8_click() {
+    var num = document.getElementById("num");
+    var n = num.value;
+    //清除前导的数字0
+    // if (n == "0") {
+    //     n = "1";
+    // } else {
+    //     n = n + "1";
+    // }
+    n = n + "8";
+    document.getElementById("num").value = n * 1;
+}
+
+function num_9_click() {
+    var num = document.getElementById("num");
+    var n = num.value;
+    //清除前导的数字0
+    // if (n == "0") {
+    //     n = "1";
+    // } else {
+    //     n = n + "1";
+    // }
+    n = n + "9";
+    document.getElementById("num").value = n * 1;
+}

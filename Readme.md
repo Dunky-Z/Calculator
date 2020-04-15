59cf7611e06380642524067dba872560baa5dbe0
如云泊 <254758318@qq.com>
2020-04-16 00:11:43 +0800

    显示数字优化

--- Readme.md
+++ Readme.md
@@ -7,3 +7,21 @@
 - 输入框禁用后为灰色背景，将其调整为白色
 
 ### 数字显示
+```javascript
+function num_1_click() {
+    var num = document.getElementById("num");
+    var n = num.value;
+    //清除前导的数字0
+    // if (n == "0") {
+    //     n = "1";
+    // } else {
+    //     n = n + "1";
+    // }
+    n = n + "1";
+    //通过因式转换传入值，去除前导的0
+    document.getElementById("num").value = n * 1;
+}
+```
+
+### 0-9数字显示的代码优化
+上个做法需要对每一个按钮都绑定一个事件函数。
--- calc.js
+++ calc.js
@@ -2,121 +2,37 @@ function init() {
     var num = document.getElementById("num");
     num.value = 0;
     num.disabled = "disabled";
+
+    var oButton = document.getElementsByTagName("input");
+    for (var i = 0; i < oButton.length; ++i) {
+        oButton[i].onclick = function() {
+            if (isNumber(this.value)) {
+                // num.value += this.value * 1;
+                if (isNull(num.value)) {
+                    num.value = this.value;
+                } else {
+                    num.value = num.value + this.value;
+                }
+            } else {
+                alert("2");
+            }
+        }
+    }
+}
+/*验证文本框是否为空或者0*/
+function isNull(n) {
+    if (n == "0" || n.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-function num_1_click() {
-    var num = document.getElementById("num");
-    var n = num.value;
-    //清除前导的数字0
-    // if (n == "0") {
-    //     n = "1";
+function isNumber(n) {
+    // if (isNaN(n) == false) {
+    //     return true; //参数n是数字
     // } else {
-    //     n = n + "1";
+    //     return false; //参数n不是数字
     // }
-    n = n + "1";
-    document.getElementById("num").value = n * 1;
-}
-
-function num_2_click() {
-    var num = document.getElementById("num");
-    var n = num.value;
-    //清除前导的数字0
-    // if (n == "0") {
-    //     n = "1";
-    // } else {
-    //     n = n + "1";
-    // }
-    n = n + "2";
-    document.getElementById("num").value = n * 1;
-}
-
-function num_3_click() {
-    var num = document.getElementById("num");
-    var n = num.value;
-    //清除前导的数字0
-    // if (n == "0") {
-    //     n = "1";
-    // } else {
-    //     n = n + "1";
-    // }
-    n = n + "3";
-    document.getElementById("num").value = n * 1;
-}
-
-function num_4_click() {
-    var num = document.getElementById("num");
-    var n = num.value;
-    //清除前导的数字0
-    // if (n == "0") {
-    //     n = "1";
-    // } else {
-    //     n = n + "1";
-    // }
-    n = n + "4";
-    document.getElementById("num").value = n * 1;
-}
-
-function num_5_click() {
-    var num = document.getElementById("num");
-    var n = num.value;
-    //清除前导的数字0
-    // if (n == "0") {
-    //     n = "1";
-    // } else {
-    //     n = n + "1";
-    // }
-    n = n + "5";
-    document.getElementById("num").value = n * 1;
-}
-
-function num_6_click() {
-    var num = document.getElementById("num");
-    var n = num.value;
-    //清除前导的数字0
-    // if (n == "0") {
-    //     n = "1";
-    // } else {
-    //     n = n + "1";
-    // }
-    n = n + "6";
-    document.getElementById("num").value = n * 1;
-}
-
-function num_7_click() {
-    var num = document.getElementById("num");
-    var n = num.value;
-    //清除前导的数字0
-    // if (n == "0") {
-    //     n = "1";
-    // } else {
-    //     n = n + "1";
-    // }
-    n = n + "7";
-    document.getElementById("num").value = n * 1;
-}
-
-function num_8_click() {
-    var num = document.getElementById("num");
-    var n = num.value;
-    //清除前导的数字0
-    // if (n == "0") {
-    //     n = "1";
-    // } else {
-    //     n = n + "1";
-    // }
-    n = n + "8";
-    document.getElementById("num").value = n * 1;
-}
-
-function num_9_click() {
-    var num = document.getElementById("num");
-    var n = num.value;
-    //清除前导的数字0
-    // if (n == "0") {
-    //     n = "1";
-    // } else {
-    //     n = n + "1";
-    // }
-    n = n + "9";
-    document.getElementById("num").value = n * 1;
+    return !isNaN(n);
 }
